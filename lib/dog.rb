1eee8cecbda6c8905d87ac770dc7adf521703a72
Michelle Olivieri <michelleo72@gmail.com>
2018-03-16 15:17:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,15 +5,11 @@ class Dog
   def name
     @dog_name
   end
-  
 
-  def breed(dog_breed)
+  def breed=(dog_breed)
     @dog_breed = dog_breed
   end
   def dog_breed
     @dog_breed
-  end
-  def dog_breed=(new_dog_breed)
-    @dog_breed = new_dog_breed
   end
 end
